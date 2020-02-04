d227a7ae1711393bbc4dcfffe5e4d919ea5b270b
philipwahlqvist <59251751+philipwahlqvist@users.noreply.github.com>
2020-02-04 13:50:37 +0100

    Add files via upload

---
+++ Expedit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    class Expedit
+    {
+        public int employeID { get; private set; }
+        public string name { get; private set; }
+        public string role { get; private set; }
+        public string password { get; private set; }
+
+        public Expedit(int employeID, string name, string role, string password)
+        {
+            this.employeID = employeID;
+            this.name = name;
+            this.role = role;
+            this.password = password;
+        }
+    }
+}
---
+++ Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    class Member
+    {
+        private int memberID { get; set; }
+        private string phoneNr { get; set; }
+        private string mail { get; set; }
+
+    }
+}
